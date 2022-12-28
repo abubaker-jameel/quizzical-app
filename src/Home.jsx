04ab390afadfdc9e2e87a321d9beeea4adfd40c5
abubaker <codecookies958@gmail.com>
2022-12-28 18:25:52 +0500

    Initial Release

---
+++ src/Home.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <>
+      <main>
+        <div>
+          <h1>Quizzical</h1>
+          <p>
+            Conquer your cluelessness with the ingenious quiz app that helps you
+            learn about the questions.
+          </p>
+          <button>Start Quiz</button>
+        </div>
+      </main>
+    </>
+  );
+}
